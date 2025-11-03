ccf9f81fef3fecdadfa805873fb8499b0ddeee6e
jui <juiyezarkar1999@gmail.com>
2025-11-03 21:02:05 +0530

    fix:cors error

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import transactionRoutes from "./routes/transactionRoutes.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
